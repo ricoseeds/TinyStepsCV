554fddb3df326ab74fff9d7332c33987dc9b9853
ricoseeds <rghchaks73@gmail.com>
2018-12-05 17:48:16 +0530

    View Img

--- TinyStepCV/main.cpp
+++ TinyStepCV/main.cpp
@@ -9,9 +9,17 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    Mat test = imread("/Users/arghachakraborty/Desktop/cats.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+//    imshow("DOF", test);
+//    imwrite("/Users/arghachakraborty/Desktop/dof_g.png", test);
+    namedWindow("DOF", CV_WINDOW_FREERATIO);
+    imshow("DOF", test);
+//    moveWindow("DOF", 0, 100);
+    waitKey();
     return 0;
 }
