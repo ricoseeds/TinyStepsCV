9959fd100efa1481ceecc0928111d7f680f83ac0
ricoseeds <rghchaks73@gmail.com>
2018-12-05 15:03:33 +0530

    Initial Commit

---
+++ TinyStepCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TinyStepCV
+//
+//  Created by Argha Chakraborty on 05/12/18.
+//  Copyright © 2018 Argha Chakraborty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
