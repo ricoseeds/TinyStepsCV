0d03ad20d23e4448402633e69585366017667933
ricoseeds <rghchaks73@gmail.com>
2018-12-06 13:57:52 +0530

    Discrete Fourier Transform on an image

--- TinyStepCV/main.cpp
+++ TinyStepCV/main.cpp
@@ -13,36 +13,36 @@
 using namespace cv;
 using namespace std;
 
+void takeDFT(Mat &source, Mat &dest);
+void showDFT(Mat &source);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-//    Mat original = imread("/Users/arghachakraborty/Desktop/cats.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
-//    Mat mod = imread("/Users/arghachakraborty/Desktop/cats.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat original = imread("/Users/arghachakraborty/Desktop/cats.jpeg", CV_LOAD_IMAGE_COLOR);
-//    Mat mod = imread("/Users/arghachakraborty/Desktop/cats.jpeg", CV_LOAD_IMAGE_COLOR);
-    Mat splitChannels[3];
-    split(original, splitChannels);
-    splitChannels[2] = Mat::zeros(splitChannels[2].size(), CV_8UC1);
-    
-    Mat out;
-    merge(splitChannels, 3, out);
-    imshow("MOD", out);
-//    imshow("Blue", splitChannels[0]);
-//    imshow("Green", splitChannels[0]);
-//    imshow("Red", splitChannels[0]);
-    
-//    for (int r = 0; r < mod.rows; r++) {
-//        for (int c = 0; c < mod.cols; c++) {
-////            mod.at<uint8_t>(r, c) += ((double) rand() / (RAND_MAX)) + 1;
-////            mod.at<Vec3b>(r, c)[0] = rand() % 255;
-////            mod.at<Vec3b>(r, c)[1] = rand() % 127;
-////            mod.at<Vec3b>(r, c)[2] = rand() % 63;
-//        }
-//    }
-//
-//    imshow("cat", original);
-//    imshow("modcat", mod);
-    waitKey();
-    
+    Mat original = imread("/Users/arghachakraborty/Desktop/cats.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat originalFloat;
+    original.convertTo(originalFloat, CV_32FC1, 1.0 / 255.0 );
+    Mat dftOriginal;
+    takeDFT(originalFloat, dftOriginal);
+    showDFT(dftOriginal);
     return 0;
+}
+
+void takeDFT(Mat &source, Mat &dest){
+    Mat originalComplex[2] = {source, Mat::zeros(source.size(), CV_32F)};
+    Mat dftReady;
+    merge(originalComplex, 2, dftReady);
+    Mat dftOriginal;
+    dft(dftReady, dftOriginal, DFT_COMPLEX_OUTPUT);
+    dest = dftOriginal;
+}
+
+void showDFT(Mat &source){
+    Mat splitArray[2] = {Mat::zeros(source.size(), CV_32F)};
+    split(source, splitArray);
+    Mat dftMagnitude;
+    magnitude(splitArray[0], splitArray[1], dftMagnitude);
+    dftMagnitude += Scalar::all(1);
+    log(dftMagnitude, dftMagnitude);
+    normalize(dftMagnitude, dftMagnitude, 0, 1, CV_MINMAX);
+    imshow("DFT", dftMagnitude);
+    waitKey();
 }
